6d861a2ad063b3d627dd3a0690fcc19508fa0299
Ali Honarvar <honarvar@gmail.com>
2012-05-25 09:34:14 -0400

    Minor

--- bin/xinput-scroll-direction.sh
+++ bin/xinput-scroll-direction.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-xinput set-button-map 'Logitech USB Optical Mouse' 1 2 3 5 4 7 6 8 9 10 11 12 13 14 15 16
-xinput set-button-map 'Mitsumi Electric Apple Optical USB Mouse' 1 2 3 5 4 7 6 8 9 10 11 12 13 14 15 16
+for mouse in \
+    'Logitech USB Optical Mouse' \
+    'Mitsumi Electric Apple Optical USB Mouse'
+do
+    xinput set-button-map "$mouse" 1 2 3 5 4 7 6 8 9 10 11 12 13 14 15 16
+done
