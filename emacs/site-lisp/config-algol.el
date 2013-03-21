e58e69d4f2aaddd4d738349f9e59ede5b5badcc7
Ali Honarvar <honarvar@gmail.com>
2013-03-21 15:01:33 -0400

    Minor change: remove c-mode hook now that it is in global.

--- emacs/site-lisp/config-algol.el
+++ emacs/site-lisp/config-algol.el
@@ -1,9 +1,7 @@
 (defalias 'perl-mode 'cperl-mode)
 
 (require 'cc-mode)
-(defun config-c-mode-hook ()
-  ;; (glasses-mode t)
-  (subword-mode t))
+;; (defun config-c-mode-hook () (glasses-mode t) (subword-mode t))
 ;; (add-hook 'c-mode-common-hook 'config-c-mode-hook)
 
 (require 'haskell-indentation)
