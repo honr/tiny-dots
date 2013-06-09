f80e60bb3f0eddb28263025bfc06e0c5bf5826eb
Ali Honarvar <honarvar@gmail.com>
2013-06-09 00:06:22 -0400

    css.clj

--- bin/css.clj
+++ bin/css.clj
@@ -5,6 +5,7 @@
   (:require [rose.file :as rfile]
             [rose.clu :as clu]
             [rose.ss-css :reload true]
+            [rose.ang-html]
             [clojure.string :as string]))
 
 (defn gen {:cli {:file String}}
@@ -22,5 +23,28 @@
                      \]))))))
     (rose.ss-css/print-tree
      (read-string (str \[ (slurp *in*) \])))))
+
+(defn colors-sample {:cli {:file String}}
+  []
+  (spit (rfile/path (rose.clu/*opts* :file))
+        (with-out-str
+
+          (rose.ang-html/print-tree
+           [:html
+            [:head nil]
+            [:body
+             (for [[color hue-sat] (sort rose.ss-css/known-colors-hue-sat)]
+               [:div
+                [:span {:style
+                        (str "display:inline-block;"
+                             "width: 10em; "
+                             "background: "
+                             (rose.ss-css/color-str hue-sat 0.5 nil))}
+                 color]
+                [:span {:style
+                        (str "display:inline-block;"
+                             "width: 10em; "
+                             "background: " color)}
+                 color]])]]))))
 
 (clu/run-command-maybe-ns *ns* "css.clj")
