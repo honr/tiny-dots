6aa3cde7e0f5c52c3c44d72257a308fbae10726c
Ali Honarvar <honarvar@gmail.com>
2013-06-14 18:25:52 -0400

    minor

--- emacs/init.el
+++ emacs/init.el
@@ -101,7 +101,7 @@
    '(mouse-wheel-progressive-speed nil)
    '(mouse-wheel-scroll-amount '(3 ((shift) . 1) ((control))))
    '(next-screen-context-lines 8)
-   '(org-agenda-files '("~/Documents/agenda.org"))
+   '(org-agenda-files '("~/agenda.org"))
    '(org-export-html-style
      "<link rel=\"stylesheet\" href=\"style.css\">")
    '(org-replace-disputed-keys t)
