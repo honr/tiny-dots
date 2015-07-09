46f1a4329aaa2ee9851438b23c87dac0486c0b42
Ali Honarvar <honarvar@gmail.com>
2015-07-09 10:14:10 -0700

    Load magit if available.

--- emacs/site-lisp/config-keys.el
+++ emacs/site-lisp/config-keys.el
@@ -164,6 +164,15 @@
                                (delete-and-extract-region
                                 (car bounds) (cdr bounds))))))))
 
+(global-set-key (kbd "C-t h")
+                (lambda ()
+                  (interactive)
+                  (let ((bounds (bounds-of-thing-at-point 'symbol)))
+                    (when bounds
+                      (insert (join-aaBb
+                               (delete-and-extract-region
+                                (car bounds) (cdr bounds))))))))
+
 (require 'browse-kill-ring)
 (defadvice yank-pop (around kill-ring-browse-maybe (arg))
   "If last action was not a yank, run `browse-kill-ring' instead."
