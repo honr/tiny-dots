27c012de48d109a9ff2de0671ec60dcfeab5e7d4
Ali Honarvar <honarvar@gmail.com>
2016-06-05 00:13:51 -0700

    minor

--- bin/xinput-scroll-direction.sh
+++ bin/xinput-scroll-direction.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-for mouse in \
-    'Logitech USB Optical Mouse' \
+function reverse_scroll_direction() {
+  local mice=(
+    'Logitech USB Optical Mouse'
     'Mitsumi Electric Apple Optical USB Mouse'
-do
-    xinput set-button-map "$mouse" 1 2 3 5 4 7 6 8 9 10 11 12 13 14 15 16
-done
+    'HID-compliant Mouse HID-compliant Mouse'
+  )
+  local mouse
+  for mouse in "${mice[@]}"; do
+    local mouse_id="$(xinput list --id-only "pointer:$mouse" 2>/dev/null)"
+    if [ -n "$mouse_id" ] ; then
+      echo "Mouse '$mouse' found."
+      xinput set-button-map "$mouse_id" 1 2 3 5 4 7 6 8 9 10 11 12 13 14 15 16
+    fi
+  done
+}
+
+reverse_scroll_direction
